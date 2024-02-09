aa11029263fd88d97b76e00fcf80e8336f04c74b
Yevhenii Matviienko <foxacid@keemail.me>
2024-02-09 07:15:13 +0200

    Update

--- _app/immutable/chunks/paths.dEntW1Om.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_h3qljk)==null?void 0:s.base)??"/favorit-2-preview";var e;const t=((e=globalThis.__sveltekit_h3qljk)==null?void 0:e.assets)??a;export{t as a,a as b};
--- _app/immutable/entry/start._sOWcSk9.js
+++
@@ -1,1 +0,0 @@
-import{g as a}from"../chunks/entry.IVXcldWb.js";export{a as start};
