68e0ee3305e4c5b25511e39d578f1f64ea2b88e7
Yevhenii Matviienko <foxacid@keemail.me>
2024-02-10 05:33:14 +0200

    Update

--- _app/immutable/chunks/stores.wXv22X-g.js
+++
@@ -1,1 +0,0 @@
-import"./scheduler.EwKZp8NQ.js";import{s as r}from"./entry.R-XjQmbd.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},a={subscribe(s){return e().page.subscribe(s)}},o={subscribe(s){return e().navigating.subscribe(s)}};export{o as n,a as p};
