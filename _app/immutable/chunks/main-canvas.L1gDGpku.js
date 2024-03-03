3ffb553dd4735243230ae2bd082b6debb5940a18
Yevhenii Matviienko <foxacid@keemail.me>
2024-03-03 18:26:28 +0200

    Update

--- _app/immutable/chunks/main-canvas.L1gDGpku.js
+++
@@ -1,1 +0,0 @@
-function r(e){return new Worker(""+new URL("../workers/main-canvas-BF_eM6Hc.js",import.meta.url).href,{name:e==null?void 0:e.name})}export{r as default};
--- _app/immutable/entry/start.7oHQrF5o.js
+++
@@ -1,1 +0,0 @@
-import{e as a}from"../chunks/entry.EAI1SeK4.js";export{a as start};
